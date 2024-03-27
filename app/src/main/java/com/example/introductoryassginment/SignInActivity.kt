13aaacc7f8ddd9bb959c5f690d7d36ea038212e8
Bettor <be0128st@gmail.com>
2024-03-27 23:04:23 +0900

    layout 조금 수정하고, 회원가입에서 입력한 데이터 넘기는 기능 추가하긴했다..

--- app/src/main/java/com/example/introductoryassginment/SignInActivity.kt
+++ app/src/main/java/com/example/introductoryassginment/SignInActivity.kt
@@ -1,24 +1,37 @@
 package com.example.introductoryassginment
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var edittext_id : EditText
+    private lateinit var editext_pw : EditText
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
         // 1. 선언부
         // xml에 만든것 중에 사용할 것들 싹다 변수 선언 해주기
+
+        // EditText 필드를 초기화
+        edittext_id = findViewById(R.id.editTextId)
+        editext_pw = findViewById(R.id.editTextPassword)
         val btn_login = findViewById<Button>(R.id.loginBtn)
         val btn_signin = findViewById<Button>(R.id.signInBtn)
-        val edittext_id = findViewById<EditText>(R.id.editTextId)
-        val editext_pw = findViewById<EditText>(R.id.editTextPassword)
+
+        // 회원가입 결과 처리를 설정
+        setResultSignUp()
+
 
         // 2. 구현부
         // 로그인(normal), 아이디와 비밀번호
@@ -31,7 +44,7 @@ class SignInActivity : AppCompatActivity() {
         // 회원가입 액티비티로 이동
         btn_signin.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
+            resultLauncher.launch(intent)
         }
     }
     // 3. 구현부에서 쓰일 함수들을 선언해주는 곳
@@ -40,8 +53,8 @@ class SignInActivity : AppCompatActivity() {
     // trim() : 공백이 있다면 제거하기 위함.
     // || : OR 연산자임.
     private fun normalToast(context: Context, inputId: String, inputPw: String) {
-        val trimmedId = inputId.trim()
-        val trimmedPw = inputPw.trim()
+        val trimmedId = edittext_id.text.toString().trim()
+        val trimmedPw = editext_pw.text.toString().trim()
         if (trimmedId.isEmpty() || trimmedPw.isEmpty()) {
             showToast(this, "아이디/비밀번호를 확인해주세요.")
         } else {
@@ -57,6 +70,19 @@ class SignInActivity : AppCompatActivity() {
     // 토스트 메세지
     private fun showToast(context: Context, message: String) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+    private fun setResultSignUp() {
+        resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+                data?.let {
+                    val id = it.getStringExtra("id") ?: ""
+                    val pw = it.getStringExtra("pw") ?: ""
+                    edittext_id.setText(id)
+                    editext_pw.setText(pw)
+                }
+            }
+        }
     }
 }
 
